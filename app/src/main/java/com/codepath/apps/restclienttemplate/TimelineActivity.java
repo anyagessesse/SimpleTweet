d3c20d96ce176c9d0d7d6c89cf18783e19e7594e
Anya Gessesse <anyagess@fb.com>
2020-06-30 10:08:53 -0700

    compose screen setup

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -62,8 +63,9 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.compose){
             //compose icon has been selected
-            Toast.makeText(this,"compose",Toast.LENGTH_SHORT).show();
             //navigate to the compose activity
+            Intent intent = new Intent(this,ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
